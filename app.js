766b0b8d6eba4183e3a60e239c9ce226bbf63219
ZeroThe2nd <nightfireluna@gmail.com>
2018-02-08 14:47:53 +0100

    -Added automatic loading -Fixed typo in Story

--- app.js
+++ app.js
@@ -8,11 +8,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const player = new Player;
     player.loadGame();
 
-    const story  = new Story(
+    const story = new Story(
         document.querySelector('#input'),
         document.querySelector('#backlog'),
         document.querySelector('#image'),
     );
 
     story.setChapter(new Ch1(player));
+
+
+// Communicate to DOM that we have JS
+    document.documentElement.setAttribute('class', 'js');
+
+    var searchFauxInput = document.querySelector('.fb-Search_FauxInput');
+    var searchBox       = document.getElementById('input');
+
+    searchBox.addEventListener('keyup', function copyInput(event) {
+        searchFauxInput.textContent = searchBox.value;
+        searchBox.setAttribute('value', searchBox.value);
+    }, false);
 });
